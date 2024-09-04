16fa762fbb286174b40257a9cd459d102a567f8a
Manoj Dixit <mndxt007@gmail.com>
2024-09-04 12:24:12 +0530

    Some progress

--- WorkDot.Api/Kernel/KernelFunctions.cs
+++ WorkDot.Api/Kernel/KernelFunctions.cs
@@ -15,13 +15,14 @@ namespace AiChatApi.KernelPlugins
         }
 
         [KernelFunction("graph_call")]
-        [Description("Fetches/Retrieves/Gets/Shows, my Emails based on the user input criteria")]
+        [Description("Fetches/Retrieves/Gets/Shows Emails based on the user input criteria")]
         public async Task<List<EmailDetails>> RetrieveEmailAsync([Description("A Microsoft Graph API query string for retrieving emails based on the user input, following the format $top=[number]&$orderby=receivedDateTime desc&$filter=[filter conditions], with rules for $top, $orderby, and $filter as specified.")] string queryParmeter)
         {
             using (var scope = _serviceProvider.CreateScope())
             {
                 var _graphService = scope.ServiceProvider.GetRequiredService<GraphService>();
-                return await _graphService.GetUserEmailsWithRawQueryAsync(queryParmeter);
+                var messages = await _graphService.GetUserEmailsWithRawQueryAsync(queryParmeter);
+                return messages;
             }
         }
 
@@ -30,6 +31,20 @@ namespace AiChatApi.KernelPlugins
         public DateTime GetCurrentDateTime()
         {
             return DateTime.Now;
+        }
+#pragma warning disable SKEXP0001
+        public class FunctionCallsFilter() : IAutoFunctionInvocationFilter
+        {
+            public async Task OnAutoFunctionInvocationAsync(AutoFunctionInvocationContext context, Func<AutoFunctionInvocationContext, Task> next)
+            {
+
+                await next(context);
+                var result = context.Result;
+                if (context.Function.Name == "graph_call")
+                {
+                    context.Terminate = true;
+                }
+            }
         }
     }
 }
